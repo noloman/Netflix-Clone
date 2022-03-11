155edeac01367736750b159ee2e5d100c5a501eb
Manuel Lorenzo <manulorenzop@gmail.com>
2022-03-11 13:44:09 +0100

    WIP add header image with gradient

---
+++ Netflix Clone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,43 @@
+//
+//  HeroHeaderUIView.swift
+//  Netflix Clone
+//
+//  Created by Manu on 11/03/2022.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "LordOfTheRingsImage")
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+}
