47758cc46a4c04b83c45b7938a88fe94e056bb2d
Manuel Lorenzo <manulorenzop@gmail.com>
2022-03-13 12:31:51 +0100

    show upcoming movies

--- Netflix Clone/View Controllers/UpcomingViewController.swift
+++ Netflix Clone/View Controllers/UpcomingViewController.swift
@@ -8,9 +8,55 @@
 import UIKit
 
 class UpcomingViewController: UIViewController {
+    private var movies = [Result]()
+    
+    private let upcomingTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTable)
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
+        
+        fetchUpcoming()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    private func fetchUpcoming() {
+        Task {
+            let movies = try? await APICaller.shared.getUpcomingMovies()
+            configure(with: movies?.results ?? [Result]())
+        }
+    }
+    
+    private func configure(with model: [Result]) {
+        self.movies = model
+        self.upcomingTable.reloadData()
+    }
+}
+
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = movies[indexPath.row].originalName ?? movies[indexPath.row].originalTitle
+        return cell
     }
 }
